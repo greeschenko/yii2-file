8cdad41c12bee216232da24e57f70c263b6100d5
Oleksiy Hryshchenko <greeschenko@gmail.com>
2021-04-30 12:29:19 +0300

    up

--- controllers/DoController.php
+++ controllers/DoController.php
@@ -164,4 +164,24 @@ class DoController extends Controller
 
         return $res;
     }
+
+    public function actionRead($path, $name)
+    {
+        $file = 'uploads/'.$path.'/'.$name;
+
+        if (file_exists($file)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename='.basename($file));
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            ob_clean();
+            flush();
+            readfile($file);
+            exit;
+        }
+    }
 }
